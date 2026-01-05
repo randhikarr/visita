51d5a5b67c080828597ba7977b09f98c6429e6b2
randhikarr <dhika030405@gmail.com>
2026-01-05 18:48:23 +0700

    wowo

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -50,6 +50,7 @@
                      <li class="nav-item">
                         <a class="nav-link" href="{{ route('tiket.saya') }}">Tiket Saya</a>
                      </li>
+                     </li>
                   <form class="form-inline my-2 my-lg-0">
                   </form>
                </div>
