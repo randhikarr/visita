4f7feeda55da0646cabcecd7770a50f8defbcc36
randhikarr <dhika030405@gmail.com>
2026-01-05 10:03:03 +0700

    Update beranda.blade.php

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -50,10 +50,6 @@
                      <li class="nav-item">
                         <a class="nav-link" href="{{ route('tiket.saya') }}">Tiket Saya</a>
                      </li>
-                  </ul>
-                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">LOG IN</a>
-                     </li>
                   <form class="form-inline my-2 my-lg-0">
                   </form>
                </div>
